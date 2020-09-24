21ed38b1b28bdbec420a8cab070a277173771969
MarkMan0 <pal.mark28@gmail.com>
2020-09-24 19:32:20 +0200

    Initial

---
+++ src/rrm_cv1_pal/include/rrm_cv1_pal/turtle_control.h
@@ -0,0 +1,49 @@
+#ifndef PROJECT_TURTLECONTROL_H
+#define PROJECT_TURTLECONTROL_H
+
+// Include ros base
+#include <ros/ros.h>
+
+// Include ros msgs
+#include <geometry_msgs/Twist.h>
+#include <turtlesim/Pose.h>
+#include <turtlesim/TeleportAbsolute.h>
+#include <turtlesim/SetPen.h>
+#include <rrm_cv1_pal/Draw.h>
+
+// main class for turtle control
+class TurtleControl
+{
+public:
+  // Constructor
+  TurtleControl();
+
+  // Callbacks
+  bool drawCallback(rrm_cv1_pal::Draw::Request& req, rrm_cv1_pal::Draw::Response& res);
+  void poseCallback(const turtlesim::Pose::ConstPtr& msg);
+
+  // Other public methods
+  void setupDrawing();
+  void publish();
+  double getRate();
+  bool getDrawingStatus();
+
+private:
+  // Consts
+  const double PUB_RATE = 100.0;
+  const double WINDOW_CENTER = 5.544444561;
+  const double WINDOW_EDGE = 11.088889122;
+
+  // member variables
+  geometry_msgs::Twist velocity_msg_;
+  bool drawing_status_;
+  turtlesim::Pose pose_msg_;
+
+  // ROS communication tools
+  ros::Publisher velocity_pub_;
+  ros::Subscriber pose_sub_;
+  ros::ServiceServer square_service_;
+  ros::ServiceClient teleport_client_;
+};
+
+#endif  // PROJECT_TURTLECONTROL_H
